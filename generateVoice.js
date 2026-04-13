ffc1415185b71c4a1b5d3c13d37f809e8f2d7c3a
SIDHARTH T <sidhartht900@gmail.com>
2026-04-13 16:30:20 +0530

    fix : audio msg related bug fixed

--- generateVoice.js
+++ generateVoice.js
@@ -1,36 +1,35 @@
-import googleTTS from "google-tts-api";
 import fs from "fs";
 import https from "https";
 
 export default async function generateVoice(text, filePath) {
-  const url = googleTTS.getAudioUrl(text, {
-    lang: "en",
-    slow: false,
-    host: "https://translate.google.com",
-  });
+  const encodedText = encodeURIComponent(text);
+
+  const url = `https://translate.google.com/translate_tts?ie=UTF-8&q=${encodedText}&tl=en&client=tw-ob`;
 
   return new Promise((resolve, reject) => {
     const file = fs.createWriteStream(filePath);
 
-    https.get(url, (response) => {
-      if (response.statusCode !== 200) {
-        reject(new Error("Failed to fetch audio"));
-        return;
-      }
+    https
+      .get(url, (res) => {
+        if (res.statusCode !== 200) {
+          reject(new Error("Failed to fetch audio"));
+          return;
+        }
 
-      response.pipe(file);
+        res.pipe(file);
 
-      file.on("finish", () => {
-        file.close(() => resolve(true));
-      });
+        file.on("finish", () => {
+          file.close(() => resolve(true));
+        });
 
-      file.on("error", (err) => {
+        file.on("error", (err) => {
+          fs.unlink(filePath, () => {});
+          reject(err);
+        });
+      })
+      .on("error", (err) => {
         fs.unlink(filePath, () => {});
         reject(err);
       });
-    }).on("error", (err) => {
-      fs.unlink(filePath, () => {});
-      reject(err);
-    });
   });
 }
