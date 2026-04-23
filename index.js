4fda2039ea86cc41b9f8a5915592003c921827e2
SIDHARTH T <sidhartht900@gmail.com>
2026-04-23 20:36:55 +0530

    fix: add daily report schedule to run at 20:40

--- index.js
+++ index.js
@@ -1375,6 +1375,8 @@ async function startBot() {
 
   cron.schedule("30 23 * * *", finalWarning, { timezone: TIMEZONE });
 
+  cron.schedule("40 20 * * *", dailyReport, { timezone: TIMEZONE });
+
   cron.schedule("0 0 * * *", dailyReport, { timezone: TIMEZONE });
 
   cron.schedule(
